520bd39b5d3896bd47134aa97ac9d6cb94a5ec58
Mohammed <alhawymohammed2@gmail.com>
2024-10-02 00:32:33 +0300

    swiper

--- js/main.js
+++ js/main.js
@@ -47,16 +47,3 @@ category_btn.onclick=Open_Categ_List
 
 
 
-
-// setting swiper
-var swiper = new Swiper(".slide-swp", {
-  autoplay: {
-    delay: 2500,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    dynamicBullets: true,
-    clickable: true,
-  },
-  loop: true,
-});
---
+++ js/swiper.js
@@ -0,0 +1,31 @@
+
+// Home slider setting
+
+var swiper = new Swiper(".slide-swp", {
+  autoplay: {
+    delay: 2500,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+    clickable: true,
+  },
+  loop: true,
+});
+
+
+// slide slide setting
+
+var swiper = new Swiper(".slide_product", {
+  slidesPerView:5,
+  spaceBetween:20,
+  autoplay: {
+    delay: 2500,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+    clickable: true,
+  },
+  loop: true,
+});
